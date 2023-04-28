80991b228881911558548ed5f5944f60e57760b1
xorvoid <xorvoid@gmail.com>
2023-04-28 15:43:40 -0500

    Switched back to LD_PRELOAD using mmap() to get a low-memory region and then use a bump-allocator

--- malloc_shim.c
+++ malloc_shim.c
@@ -1,16 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 #define SIZE (8*1024*1024)
-static char mem[SIZE];
-static char *ptr = mem;
+#define ADDR ((char*)0x70000000)
+#define ADDR_END (ADDR + SIZE)
+
+static int    init   = 0;
+static char * ptr    = ADDR;
+
+static void ensure_init(void)
+{
+  if (init) return;
+
+  void *mem = mmap(ADDR, SIZE, PROT_EXEC|PROT_WRITE|PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  if (mem != ADDR) {
+    fprintf(stderr, "Failed to map memory to %x in malloc_shim\n", (size_t)ADDR);
+    exit(42);
+  }
+
+  init = 1;
+}
 
 void *malloc(size_t sz)
 {
+  ensure_init();
+
   sz = (sz+16)&~15; /* align up */
 
-  size_t left = SIZE - (ptr - mem);
+  size_t left = ADDR_END - ptr;
   if (sz > left) abort(); /* out-of-memory */
 
   char *ptr_ret = ptr;
@@ -21,14 +41,8 @@ void *malloc(size_t sz)
 
 void *calloc(size_t num, size_t sz)
 {
-  size_t total_size = num * sz;
-  total_size = (total_size+16)&~15; /* align up */
-
-  /* Call directly to sbrk() on i386 linux */
-  /*void *ret = (void*)syscall(45, total_size);*/
-  void *ret = malloc(total_size);
-
-  memset(ret, 0, total_size);
+  void *ret = malloc(sz);
+  memset(ret, 0, sz);
   return ret;
 }
 
